5ef3ade5efb8f3bdedf85c236568594c458f32c4
Ibrim300 <133498541+Ibrim300@users.noreply.github.com>
2023-06-19 07:21:46 -0700

    Add files via upload

---
+++ cryptomoney.js
@@ -0,0 +1,25 @@
+ 
+// const getStartedButton = document.getElementById('get-started');
+
+// getStartedButton.addEventListener('click', () => {
+//   // Add functionality here to handle the 'Get Started' button click
+// });
+
+// // 111
+// // Optional click event for tracking
+// document.querySelector('.get-btn').addEventListener('click', function() {
+//     console.log('Download button clicked');
+//   });
+
+//   //  22
+//   const button = document.querySelector('button');
+// button.addEventListener('click', function() {
+//   alert('You clicked the Read More button!');
+// });
+
+// // 333
+// const title = document.querySelector('h1');
+// const image = document.querySelector('img');
+
+// title.textContent = "New Title";
+// image.src = "new-image.png";
